8fbed562dfaa11369f33ae738c21a8132e5242d3
lokistarwars <89352187+lokistarwars@users.noreply.github.com>
2021-08-26 11:45:50 +0530

    Add files via upload

---
+++ PhoenixMiner-5.5c/start.bat
@@ -0,0 +1,3 @@
+@echo off
+PhoenixMiner.exe -pool ssl://eu1-etc.ethermine.org:5555 -pool2 ssl://eu1-etc.ethermine.org:5555 -ewal 0x6ff85749ffac2d3a36efa2bc916305433fa93731.osacwrfxcb7x8nc -gpow 96
+pause
